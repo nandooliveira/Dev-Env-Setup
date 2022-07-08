76f0ae8ce59d7d2764dcac346f213030744cb6a4
Fernando Oliveira <nandooliveira.al@gmail.com>
2022-07-08 19:22:13 -0300

    Update install.s

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ pushd ~/Dev-Env-Setup
 ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -s "$(pwd)/init.vim" ~/.config/nvim/init.vim
 ln -s "$(pwd)/.gitignore" ~/.gitignore
+
+nvim +silent  +PlugInstall +qall
